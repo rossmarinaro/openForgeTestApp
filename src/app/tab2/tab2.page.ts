1290f76118d519aff6264ff5a5ceb106a2736da7
ross marinaro <rossmarinaro@gmail.com>
2022-05-21 12:46:06 -0400

    'update'

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { User } from '../user-model';
+import { Octokit } from 'octokit';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,63 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  users: User[] = [];
+
+  constructor(@Inject(DOCUMENT) document: Document) {}
+
+  async ngOnInit()
+  { 
+    //get user accounts
+
+    const octokit = new Octokit(),
+    accounts = await octokit.request('GET /users', {});
+
+    accounts.data.map(async account => {
+      
+      const user = await octokit.request(`GET /users/${account.login}`, {
+        username: 'USERNAME'
+      });
+
+    //push account to users array
+
+      this.users.push({
+        id: account.id,
+        public_repos: user.data.public_repos,
+        login: account.login,
+        avatar_url: account.avatar_url,
+        html_url: account.html_url,
+        repos_url: account.repos_url
+      });
+
+
+    });
+  }
+
+//-------------------------- quick helpers
+
+  getElementById(element: string) 
+  {
+    return (<HTMLInputElement>document.getElementById(element));
+  }
+
+  createElement(type: any, element: string)
+  {
+    return (<typeof type>document.createElement(element));
+  }
+
+  //-------------------------- submit / apply search results
+
+  submitSearch()
+  {
+    for (let user of this.users)
+      if (this.getElementById('search-user').value === user.login)
+      {
+        let avatar = this.getElementById('user-avatar').appendChild(this.createElement(typeof HTMLIonImgElement, 'ion-img'));   
+        avatar.src = user.avatar_url;
+        this.getElementById('user-login').innerHTML = user.login;
+      }
+
+    return false;
+  }
 
 }
